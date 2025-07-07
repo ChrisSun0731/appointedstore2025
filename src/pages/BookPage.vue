a46c4359bdbb081096fff166888bc28776daeb19
ChrisSun0731 <chris20090731@gmail.com>
2025-07-07 21:57:56 +0800

    update book and map page

--- src/pages/BookPage.vue
+++ src/pages/BookPage.vue
@@ -1,51 +1,32 @@
 <template>
-  <q-page class="q-pa-md">
-    <q-card flat bordered class="q-pa-xl q-mx-auto shadow-2" style="max-width: 700px; width: 100%">
-      <q-card-section class="column items-center">
-        <q-img
-          src="/icons/testlogo.svg"
-          alt="testlogo"
-          style="max-width: 40%; max-height: 40%"
-          fit="contain"
-        />
-        <div class="text-h4 text-center q-mt-md">
-          <br />
-          <span v-if="$q.screen.gt.xs" class="q-pl-sm"
-            ><strong>建北中成四校聯合特約商店特約本</strong></span
-          >
-          <span v-else><strong>特約本</strong></span>
-          <br />
-          <br />
-        </div>
-      </q-card-section>
+  <q-page class="q-pa-none column">
+    <div class="q-pa-md q-pt-xl q-pb-none flex flex-center column">
+      <div class="text-h4 text-center q-mt-md">
+        <br />
+        <span v-if="$q.screen.gt.xs" class="q-pl-sm">
+          <strong>建北中成四校聯合特約商店特約本</strong>
+        </span>
+        <span v-else><strong>特約本</strong></span>
+        <br />
+        <br />
+      </div>
+    </div>
 
-      <q-separator spaced />
+    <q-separator spaced />
 
-      <q-card-section class="iframe-section">
-        <iframe
-          src="https://drive.google.com/file/d/10Y1gx2zgEsFyaQ3O2aM_JeHre5uf73-G/preview"
-          class="iframe-full"
-        ></iframe>
-      </q-card-section>
-    </q-card>
+    <div class="q-mx-sm" style="flex: 1; overflow: hidden">
+      <iframe
+        src="https://drive.google.com/file/d/10Y1gx2zgEsFyaQ3O2aM_JeHre5uf73-G/preview"
+        width="100%"
+        height="100%"
+        style="border: none; border-radius: 6px"
+      ></iframe>
+    </div>
   </q-page>
 </template>
 
 <style scoped>
-.iframe-section {
-  position: relative;
-  height: 60vh;
-  padding: 0;
-  overflow: hidden;
-}
-
-.iframe-full {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border: none;
-  border-radius: 3%;
+.q-page {
+  height: 100vh;
 }
 </style>
--- src/pages/MapPage.vue
+++ src/pages/MapPage.vue
@@ -1,37 +1,29 @@
 <template>
-  <q-page class="q-pa-md">
-    <q-card flat bordered class="q-pa-xl q-mx-auto shadow-2" style="max-width: 700px; width: 100%">
-      <q-card-section class="column items-center">
-        <q-img
-          src="/icons/testlogo.svg"
-          alt="testlogo"
-          style="max-width: 40%; max-height: 40%"
-          fit="contain"
-        />
-        <div class="text-h4 text-center q-mt-md">
-          <span v-if="$q.screen.gt.xs" class="q-pl-sm"
-            ><strong>建北中成四校聯合特約商店地圖</strong></span
-          >
-          <span v-else><strong>特約地圖</strong></span>
-          <br />
-          <br />
-        </div>
-      </q-card-section>
+  <q-page class="q-pa-none column">
+    <div class="q-pa-md q-pt-xl q-pb-none flex flex-center column">
+      <div class="text-h4 text-center q-mt-md">
+        <span v-if="$q.screen.gt.xs" class="q-pl-sm">
+          <strong>建北中成四校聯合特約商店地圖</strong>
+        </span>
+        <span v-else><strong>特約地圖</strong></span>
+      </div>
+    </div>
 
-      <q-separator spaced />
-      <q-card-section class="q-pa-md">
-        <div class="q-gutter-md">
-          <iframe
-            src="https://www.google.com/maps/d/embed?mid=1L5C5220pdnkEFRYWXg4yKGdhB8Ic2co&ehbc=2E312F"
-            width="100%"
-            height="600"
-            style="max-width: 100%; max-height: 100%; border: none; border-radius: 3%"
-            fit="contain"
-          ></iframe>
-        </div>
-      </q-card-section>
-    </q-card>
+    <q-separator spaced />
+
+    <div class="q-mx-sm" style="flex: 1; overflow: hidden">
+      <iframe
+        src="https://www.google.com/maps/d/embed?mid=1L5C5220pdnkEFRYWXg4yKGdhB8Ic2co&ehbc=2E312F"
+        width="100%"
+        height="100%"
+        style="border: none; border-radius: 6px"
+      ></iframe>
+    </div>
   </q-page>
 </template>
 
-<style scoped></style>
+<style scoped>
+.q-page {
+  height: 100vh;
+}
+</style>
