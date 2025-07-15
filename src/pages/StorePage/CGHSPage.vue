05243b82020772c9cf7f84d36ddf8cefd0700785
ChrisSun0731 <chris20090731@gmail.com>
2025-07-15 20:40:59 +0800

    Use CGHSlogo instead of CKHSlogo in CGHSPage.vue

--- src/pages/StorePage/CGHSPage.vue
+++ src/pages/StorePage/CGHSPage.vue
@@ -3,8 +3,8 @@
     <q-card flat bordered class="q-pa-xl q-mx-auto shadow-2" style="max-width: 700px; width: 100%">
       <q-card-section class="column items-center">
         <q-img
-          src="../../../public/icons/ckhslogo.png"
-          alt="cklogo"
+          src="../../../public/icons/cghslogo.png"
+          alt="cglogo"
           style="max-width: 40%; max-height: 40%"
           fit="contain"
         />
